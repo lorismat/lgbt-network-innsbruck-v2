6ff18664307dbaab4db15ea9b253114bc570af42
Loris M <loris.mattioni@gmail.com>
2023-06-12 11:21:29 +0200

    init

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div  class="p-12">
+    <h1>HEY</h1>
+    <TestFormKit />
+  </div>
+</template>
---
+++ components/TestFormKit.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <FormKit
+      type="select"
+      name="state"
+      label="State"
+      :options="{
+        ca: 'California',
+        ny: 'New York',
+        va: 'Virginia'
+      }"
+      v-model="state"
+    />
+
+    {{ state }}
+  </div>
+</template>
+
+<script setup>
+const state = ref('ca')
+
+</script>
